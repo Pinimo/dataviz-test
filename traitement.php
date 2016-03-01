8d066a569eca4ebcb82d90c2c474b2f3ff885cee
Pinimo <pnm@live.fr>
2016-03-01 09:57:09 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,3 +1,38 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Dataviz course</title>
+  <meta name="description" content="Basics to produce web dataviz">
+  <meta name="author" content="Pinimo et al. (2016)">
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css" rel="stylesheet" integrity="sha256-rgHoMgF45/9e2kvxfvR0KarwQNw5CRqgxbrthGpeUuc= sha512-CGGV53FSdrXrjyGCNk04p+xuna3CbE33n773A0EEmqtcN8W3NaFsR0vSQcbll5dpSS90d3i2Zh3jFX/j46hzJA==" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.1/nv.d3.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="js/json2.min.js"></script>
+    <script type="text/javascript">
+        window.__ltie9 = true;
+    </script>
+  <![endif]-->
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.15/d3.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.3.0/lodash.min.js"></script>
+</head>
+
+<body class="container">
+  <div class="jumbotron">
+    <h1>Datastories</h1>
+    <p>Identité</p>
+  </div>
+
 <p>Bonjour !</p>
 
 <p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['pied']; ?> !</p>
+</body>
+</html>
